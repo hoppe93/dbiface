b939d19ff50cdedef9907ccfdd42276594c8d202
Mathias Hoppe <mathias.hoppe@epfl.ch>
2022-03-24 13:53:33 +0100

    Fix problem with __exit__()

--- Database.py
+++ Database.py
@@ -19,7 +19,7 @@ class Database:
         return self
 
 
-    def __exit__(self):
+    def __exit__(self, exc_type, exc_value, traceback):
         """
         Called when leaving a "with" statement.
         """
